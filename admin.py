873a25a3352af12c4f2c0e1e78290203f17634a5
AVINEESHAREDDY <98671253+AVINEESHAREDDY@users.noreply.github.com>
2023-03-08 14:11:08 -0500

    Add files via upload

--- admin.py
+++ admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 
-from .models import Contact
+from .models import Listing
 
-class ContactAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'listing', 'email', 'contact_date')
-    list_display_links = ('id', 'name')
-    search_fields = ('name', 'email', 'listing')
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor')
+    list_display_links = ('id', 'title')
+    list_filter = ('realtor',)
+    list_editable = ('is_published',)
+    search_fields = ('title', 'description', 'address', 'city', 'state', 'zipcode', 'price')
     list_per_page = 25
 
-admin.site.register(Contact, ContactAdmin)
+admin.site.register(Listing, ListingAdmin)
